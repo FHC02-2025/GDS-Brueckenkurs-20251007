39e35b9df98f9ca774b3f8891b992e8ceeffaa00
Daniel Zettel <daniel.zettel@lv.campus02.at>
2025-10-07 17:33:04 +0200

    Vorbereitung (Angaben) Brückenkurs

---
+++ src/TagesabschnittApp.java
@@ -0,0 +1,17 @@
+public class TagesabschnittApp {
+
+    /**
+     * Aufgabe: Schreibe den Tagesabschnitt aufgrund der Stunde auf die Konsole
+     * 6-9 -> Früh
+     * 9-12 -> Vormittag
+     * 12-16 -> Nachmittag
+     * 16-20 -> Abend
+     * 20-6 -> Nacht
+     *
+     * Zusatz: Sollte die Stunde kleiner als 0 oder größer als 23 sein,
+     * dann soll "Falsche Stunde" auf die Konsole geschrieben werden
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/UebungsbeispieleLV1.java
@@ -0,0 +1,8 @@
+/**
+ * Übungsbeispiel 1
+ * Link: "https://moodle.campus02.at/pluginfile.php/296892/mod_resource/content/1/%C3%9Cbungsbeispiele_LV1.pdf"
+ *
+ */
+public class UebungsbeispieleLV1 {
+
+}
